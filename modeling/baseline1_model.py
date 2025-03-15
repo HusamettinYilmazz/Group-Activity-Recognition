f834a76f8a74392f569bff8bf0974bc89da65123
Husam <husamahmed616@gmail.com>
2025-03-15 14:21:09 +0000

    add baseline1 model

---
+++ modeling/baseline1_model.py
@@ -0,0 +1,23 @@
+import torch 
+import torch.nn as nn
+import torchvision.models as models
+
+
+class GroupActivity(nn.Module):
+    def __init__(self, out_features):
+        super().__init__()
+        self.resnet50 = models.resnet50(weights="DEFAULT")
+        self.resnet50.fc = nn.Linear(in_features= self.resnet50.fc.in_features, out_features=out_features)
+
+        for param in self.resnet50.parameters():
+            param.requires_grad = False
+
+        for param in self.resnet50.fc.parameters():
+            param.requires_grad = True
+
+    def forward(self, x):
+        return self.resnet50(x)
+
+
+if __name__ == "__main__":
+    gar = GroupActivity(out_features= 8)
