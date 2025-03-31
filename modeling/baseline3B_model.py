0daafa599b59800766975956308bdaf06d8ff21a
Husam <husamahmed616@gmail.com>
2025-03-31 13:12:27 +0000

    Complete & test Basline3-B (cropped players from a Single-Frame Based Group activity calssifier)

---
+++ modeling/baseline3B_model.py
@@ -0,0 +1,52 @@
+import os 
+import sys
+import torch 
+import torch.nn as nn
+import torchvision.models as models
+from baseline1_model import GroupActivity
+
+ROOT = "/teamspace/studios/this_studio/Group-Activity-Recognition"
+sys.path.append(ROOT)
+
+MODEL_ROOT = '/teamspace/studios/this_studio/Group-Activity-Recognition/modeling/outputs/Baseline 3/V1.0/epoch8_model.pth'
+
+class GroupActivity3B(nn.Module):
+    def __init__(self, out_features):
+        super().__init__()
+        self.back_bone = self.load_pretrained_model()
+        self.fc = nn.Sequential(
+            nn.Linear(in_features= self.back_bone.resnet50.fc.in_features, out_features=1024),
+            nn.ReLU(),
+            nn.Linear(in_features=1024, out_features=out_features)
+            )
+        
+        self.back_bone = nn.Sequential(*list(self.back_bone.resnet50.children())[:-1])
+        for param in self.back_bone.parameters():
+            param.requires_grad = False
+        self.max_pool = nn.AdaptiveMaxPool2d((1, 2048))  ## from (12, 2048) to (1, 2048)
+        
+
+    def forward(self, x):
+        return self.fc(x)
+    
+    def feat_extraction(self, x):
+        # x = x.to(self.device)
+        self.eval()
+        with torch.no_grad():
+            feat_vectors = self.back_bone(x)
+            feat_vectors = feat_vectors.permute(2, 3, 0, 1)
+            pooled_features = self.max_pool(feat_vectors)
+            pooled_features = pooled_features.view(-1)
+            return pooled_features
+            
+    def load_pretrained_model(self):
+        check_point = torch.load(MODEL_ROOT, map_location= torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
+        model = GroupActivity(out_features=9)
+        model.load_state_dict(check_point['model'])
+        
+        return model
+    
+
+if __name__ == "__main__":
+    gar = GroupActivity3B(out_features= 8)
+
--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,17 +1,26 @@
 import os 
+import sys
+
 import pickle
 from PIL import Image
 from albumentations.augmentations import crops
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 import cv2
+import numpy as np
 
 import torch
+import torch.nn as nn
 from torch.utils.data import Dataset
 
 
 ROOT = "/teamspace/studios/this_studio/Group-Activity-Recognition"
+# sys.path.append(ROOT)
 
 from .helper import load_config
 from .boxinfo import BoxInfo
+from modeling.baseline3B_model import GroupActivity3B ## reput the 2 dots ..modeling
+
 
 group_activity_categories = ["r_set", "r_spike" , "r-pass", "r_winpoint", "l_winpoint", "l-pass", "l-spike", "l_set"]
 group_activity_labels = {categ:idx for idx, categ in enumerate(group_activity_categories)}
@@ -21,14 +30,17 @@ person_activity_categories = [categ.lower() for categ in person_activity_categor
 person_activity_labels = {categ:idx for idx, categ in enumerate(person_activity_categories)}
 
 class GroupActivityRecognitionDataset(Dataset):
-    def __init__(self, videos_path, annot_path, split, crop=False, transform=None):
+    def __init__(self, videos_path, annot_path, split, crop=False, all_players_once=False, transform=None):
         super().__init__()
         self.transform = transform
         self.data = []
-        self.load_frame_label_pair(videos_path, annot_path, split, crop, transform=self.transform)
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model = GroupActivity3B(out_features=1).to(self.device)
+        
+        self.load_frame_label_pair(videos_path, annot_path, split, crop, all_players_once)
 
 
-    def load_frame_label_pair(self, videos_path, annot_path, split, crop, transform):
+    def load_frame_label_pair(self, videos_path, annot_path, split, crop, all_players_once):
         split = [str(i) for i in split]
         
         
@@ -40,14 +52,29 @@ class GroupActivityRecognitionDataset(Dataset):
             for clip in annot_file[video]:
                 frame_path = os.path.join(video_dir, clip, f"{clip}.jpg")
                 
-                if crop:
-                    for player in annot_file[video][clip]['frame_boxes_dct'][int(clip)]:
-                        box_info = player.box
-                        category = player.category
-                        label = torch.zeros(len(person_activity_categories)) ## One hot encoding
-                        label[person_activity_labels[category]] = 1
-
-                        self.data.append((frame_path, box_info, label))
+                if crop:                    
+                    if all_players_once:
+                        
+                        player_boxes = []
+                        for player in annot_file[video][clip]['frame_boxes_dct'][int(clip)]:
+                            player_boxes.append(player.box)
+                            
+                        category = annot_file[video][clip]['category']
+                        # label = [1 if cur_label == label else 0 for cur_label in config.model['class_labels']] ## One hot encoding
+                        label = torch.zeros(len(group_activity_categories)) ## One hot encoding
+                        label[group_activity_labels[category]] = 1
+                        
+                        self.data.append((frame_path, player_boxes, label))
+                        
+                    else:
+                        for player in annot_file[video][clip]['frame_boxes_dct'][int(clip)]:
+                            box_info = [player.box]
+                            category = player.category
+                            label = torch.zeros(len(person_activity_categories)) ## One hot encoding
+                            label[person_activity_labels[category]] = 1
+    
+                            self.data.append((frame_path, box_info, label))
+                    
                 else:
                     category = annot_file[video][clip]['category']
                     # label = [1 if cur_label == label else 0 for cur_label in config.model['class_labels']] ## One hot encoding
@@ -61,18 +88,29 @@ class GroupActivityRecognitionDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-
         frame_path, box_info, label = self.data[idx]
-        if box_info:
-            image = cv2.imread(frame_path)
+        image = cv2.imread(frame_path)
+
+        if len(box_info) > 1:
+            crops = []            
+            for box in box_info:
+                x1, y1, x2, y2= box[0], box[1], box[2], box[3]
+                crop = image[y1:y2, x1:x2]
+                crop = self.transform(image=crop)['image'] if self.transform else crop
+                crops.append(crop)
+                
+            stacked_crops = np.stack(crops, axis=0)
+            crops = torch.tensor(stacked_crops, dtype=torch.float32).to(self.device)
+            
+            image = self.model.feat_extraction(crops)  ## both of feature extraction and max pooling applied
+            
+        elif len(box_info) == 1:
+            box_info = box_info[0]
             x1, y1, x2, y2= box_info[0], box_info[1], box_info[2], box_info[3]
             crop = image[y1:y2, x1:x2]
             image = self.transform(image=crop)['image'] if self.transform else crop
             
-            # return (crop, label)
         else:
-            image = cv2.imread(frame_path)
-            # frame = torch.tensor(frame, dtype=torch.float32).permute(2, 0, 1)
             image = self.transform(image=image)["image"] if self.transform else image
             
         return (image, label)
@@ -110,14 +148,33 @@ def load_and_print_pkl_tree(pkl_path):
     print_one_branch(data)
 
 if __name__ == "__main__":
-    config_path = os.path.join(ROOT, "modeling/configs/baseline3.yaml")
+    config_path = os.path.join(ROOT, "modeling/configs/baseline3B.yaml")
     config = load_config(config_path)
     # print(config.model)
     annot_path = os.path.join(ROOT, config.data["annot_path"])
     videos_path = os.path.join(ROOT, config.data["videos_path"])
     split = config.data['video_splits']['train']
 
-    group_act = GroupActivityRecognitionDataset(videos_path, annot_path, split, crop=True)
+    train_transform = A.Compose([
+        A.Resize(224, 224),
+        A.OneOf([
+            A.GaussianBlur(blur_limit=(3, 7)),
+            A.ColorJitter(brightness=0.2),
+            A.RandomBrightnessContrast(),
+            A.GaussNoise()
+        ], p=0.9),
+        A.OneOf([
+            A.HorizontalFlip(),
+            A.VerticalFlip(),
+        ], p=0.05),
+        A.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        ),
+        ToTensorV2()
+    ])
+
+    group_act = GroupActivityRecognitionDataset(videos_path, annot_path, split, crop=True, all_players_once=False, transform=train_transform)
     print(group_act.__getitem__(idx=0))
 
     # load_and_print_pkl_tree(os.path.join(ROOT, config.data['annot_path']))
