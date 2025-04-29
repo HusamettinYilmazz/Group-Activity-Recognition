fb8e8773338cc672a719f91585bd817938a409e4
Husamettin Yilmaz <husamahmed616@gmail.com>
2025-04-29 16:52:40 +0000

    add- baseline7 (full version of two stage LSTM Group Activity Recognition)

--- modeling/train.py
+++ modeling/train.py
@@ -17,7 +17,7 @@ sys.path.append(ROOT)
 from baseline1_model import GroupActivity
 from baseline3B_model import GroupActivity3B
 from baseline4_model import SequentialGroupActivity
-from baseline6_model import SequentialGroupActivityPooledPersons
+from baseline7_model import SequentialGroupActivityPooledPersons
 
 from utils import load_config
 from utils import GroupActivityRecognitionDataset
@@ -183,7 +183,7 @@ def train_model(config, checkpoint_path=None):
 
 
 if __name__ == "__main__":
-    config_path = os.path.join(ROOT, "configs/baseline6.yaml")
+    config_path = os.path.join(ROOT, "configs/baseline7.yaml")
     config = load_config(config_path)
     
     checkpoint_path = ''
